489def6a881691aad0a652034eaf2aa0e07966e6
datht4889 <121657499+datht4889@users.noreply.github.com>
2023-05-12 09:28:48 +0700

    Create TestPassingParameter.java

---
+++ Lab03/passing-parameter/TestPassingParameter.java
@@ -0,0 +1,36 @@
+package passing_parameter.Aims;
+
+public class TestPassingParameter {
+    public static void main(String[] args){
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+        
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+
+    }
+
+    public static void swap(Wrapper o1, Wrapper o2){
+        DigitalVideoDisc tmp = o1.disc;
+        o1.disc = o2.disc;
+        o2.disc = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title){
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
+
+class Wrapper{
+    DigitalVideoDisc disc;
+
+    public Wrapper(DigitalVideoDisc disc){
+        this.disc = disc;
+    }
+}
